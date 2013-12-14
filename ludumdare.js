f51023a1539d2c9042ef3b07177eaf9aa8a8a124
Lawrence Neal <lawrencewneal@gmail.com>
2013-12-13 21:00:30 -0800

    STARS

--- ludumdare.js
+++ ludumdare.js
@@ -152,6 +152,65 @@ function init() {
   var mesh = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { map: texture, ambient: 0xbbbbbb } ) );
   scene.add( mesh );
 
+  /// STARS
+  // stars
+
+  var radius = 500;
+  var i, r = radius, starsGeometry = [ new THREE.Geometry(), new THREE.Geometry() ];
+
+  for ( i = 0; i < 250; i ++ ) {
+
+    var vertex = new THREE.Vector3();
+    vertex.x = Math.random() * 2 - 1;
+    vertex.y = Math.random() * 2 - 1;
+    vertex.z = Math.random() * 2 - 1;
+    vertex.multiplyScalar( r );
+
+    starsGeometry[ 0 ].vertices.push( vertex );
+
+  }
+
+  for ( i = 0; i < 1500; i ++ ) {
+
+    var vertex = new THREE.Vector3();
+    vertex.x = Math.random() * 2 - 1;
+    vertex.y = Math.random() * 2 - 1;
+    vertex.z = Math.random() * 2 - 1;
+    vertex.multiplyScalar( r );
+
+    starsGeometry[ 1 ].vertices.push( vertex );
+
+  }
+
+  var stars;
+  var starsMaterials = [
+    new THREE.ParticleSystemMaterial( { color: 0x555555, size: 2, sizeAttenuation: false } ),
+    new THREE.ParticleSystemMaterial( { color: 0x555555, size: 1, sizeAttenuation: false } ),
+    new THREE.ParticleSystemMaterial( { color: 0x333333, size: 2, sizeAttenuation: false } ),
+    new THREE.ParticleSystemMaterial( { color: 0x3a3a3a, size: 1, sizeAttenuation: false } ),
+    new THREE.ParticleSystemMaterial( { color: 0x1a1a1a, size: 2, sizeAttenuation: false } ),
+    new THREE.ParticleSystemMaterial( { color: 0x1a1a1a, size: 1, sizeAttenuation: false } )
+  ];
+
+  for ( i = 10; i < 30; i ++ ) {
+
+    stars = new THREE.ParticleSystem( starsGeometry[ i % 2 ], starsMaterials[ i % 6 ] );
+
+    stars.rotation.x = Math.random() * 6;
+    stars.rotation.y = Math.random() * 6;
+    stars.rotation.z = Math.random() * 6;
+
+    s = i * 10;
+    stars.scale.set( s, s, s );
+
+    stars.matrixAutoUpdate = false;
+    stars.updateMatrix();
+
+    scene.add( stars );
+
+  }
+  //
+
   var BLACK = 0x000000;
   var RED = 0xFF0000;
   var BLUE = 0x0000FF;
