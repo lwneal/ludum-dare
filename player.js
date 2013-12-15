757294b4e30d1bc9ab2c52c0b3571dec7ac1e0f7
Lawrence Neal <lawrencewneal@gmail.com>
2013-12-15 12:39:45 -0800

    Particle system for player ship

--- player.js
+++ player.js
@@ -1,3 +1,4 @@
+var NUM_PARTICLES = 1000;
 var PlayerShip = (function(){
   var init = function() {
     this.r = 5;
@@ -6,9 +7,39 @@ var PlayerShip = (function(){
     this.mesh.scale.set(this.r, this.r, this.r);
     scene.add(this.mesh);
 
+    this.particles = new THREE.Geometry();
+    this.particleMaterial = new THREE.ParticleSystemMaterial({
+      color: 0xFF0000, 
+      size: 12, 
+      sizeAttenuation: false, 
+      fog: false,
+      blending: THREE.AdditiveBlending,
+      transparent: true
+    });
+    for (var i = 0; i < NUM_PARTICLES; i++) {
+      var particlePos = new THREE.Vector3(rand(-1,1), rand(-1,1), rand(-1,1));
+      this.particles.vertices.push(particlePos);
+    }
+    this.particleSystem = new THREE.ParticleSystem(this.particles, this.particleMaterial);
+    this.particleSystem.sortParticles = true;
+    scene.add(this.particleSystem);
+
     this.missiles_left = 1;
 
     this.bounds = {obj: this};
+  };
+
+  var updateParticles = function(particles) {
+    for (var i = 0; i < NUM_PARTICLES; i++) {
+      var particlePos = particles.vertices[i];
+      if (Math.random() < 0.1) { 
+        particlePos.copy(PlayerShip.mesh.position);
+      }
+
+      particlePos.x += rand(-1, 1);
+      particlePos.y += rand(-1, 1);
+      particlePos.z += rand(-1, 1);
+    }
   };
 
   var update = function(scale) {
@@ -71,7 +102,8 @@ var PlayerShip = (function(){
       this.mesh.position.z -= BOUNDS.height;
     }
     */
-        
+
+    updateParticles(this.particles);
   };
 
   return {
