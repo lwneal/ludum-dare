6173ac106ce7f690ff8ff20aa56d79dd2631b7e5
Lawrence Neal <lawrencewneal@gmail.com>
2013-12-14 17:55:50 -0800

    Comment out multiple-render

--- ludumdare.js
+++ ludumdare.js
@@ -49,15 +49,9 @@ function init() {
   directionalLight.position.set( 1, 1, 0.5 ).normalize();
   scene.add( directionalLight );
 
-<<<<<<< HEAD
-  renderer = new THREE.WebGLRenderer( { alpha: true } );
-  renderer.autoClear = false;
-  renderer.setClearColor(BLACK, 1.0 );
-=======
   renderer = new THREE.WebGLRenderer( { alpha: false } );
   renderer.autoClear = false;
   renderer.setClearColor( BLACK, 1 );
->>>>>>> a2a674741736a16601a62eac4a4d15d781aed3dd
   renderer.setSize( window.innerWidth, window.innerHeight );
 
   container.innerHTML = "";
@@ -205,22 +199,25 @@ function animate(timestamp) {
   stats.update();
 
 }
+/*
 function renderWithOffset(scene, camera, x, y, z) {
+  var translateMat = new THREE.Matrix4();
+  translateMat.makeTranslation(x,y,z);
 
-  var t = new THREE.Matrix4();
-  t.makeTranslation(x,y,z);
+  var currentMat = new THREE.Matrix4();
+  currentMat.copy(camera.matrixWorld);
+  translateMat.multiply(camera.matrixWorld);
 
-  var m = new THREE.Matrix4(PlayerShip.mesh.matrixWorld);
+  var newCam = camera.clone();
+  newCam.matrixAutoUpdate = false;
 
-  var camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 1, 20000 );
-  camera.position.x += x;
-  camera.position.y += y;
-  camera.position.z += z;
-  // camera.setWorldMatrix or something
-  renderer.render(scene, camera);
+  newCam.matrix = translateMat;
+  renderer.render(scene, newCam);
 }
+*/
   
 function update_overlay(mesh) {
+  /*
   var p, v, percX, percY, left, top;
 
   // this will give us position relative to the world
@@ -244,11 +241,12 @@ function update_overlay(mesh) {
   $('#overlay')
       .css('left', (left - $('#overlay').width() / 2) + 'px')
       .css('top', (top - $('#overlay').height() / 2) + 'px');
+  */
 }
 
 function render() {
   renderer.clear();
-  renderWithOffset(scene, camera, 0, 1000, 0);
+  //renderWithOffset(scene, camera, BOUNDS.width, 0, 0);
   renderer.render( scene, camera );
 }
 
