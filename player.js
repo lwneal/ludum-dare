a82c9b83eec3b58db036683b06de3b37aaf6e6b3
Lawrence Neal <lawrencewneal@gmail.com>
2013-12-15 13:23:18 -0800

    Refactor particle system into fire.js

--- player.js
+++ player.js
@@ -7,39 +7,11 @@ var PlayerShip = (function(){
     this.mesh.scale.set(this.r, this.r, this.r);
     scene.add(this.mesh);
 
-    this.particles = new THREE.Geometry();
-    this.particleMaterial = new THREE.ParticleSystemMaterial({
-      color: 0xFF0000, 
-      size: 12, 
-      sizeAttenuation: false, 
-      fog: false,
-      blending: THREE.AdditiveBlending,
-      transparent: true
-    });
-    for (var i = 0; i < NUM_PARTICLES; i++) {
-      var particlePos = new THREE.Vector3(rand(-1,1), rand(-1,1), rand(-1,1));
-      this.particles.vertices.push(particlePos);
-    }
-    this.particleSystem = new THREE.ParticleSystem(this.particles, this.particleMaterial);
-    this.particleSystem.sortParticles = true;
-    scene.add(this.particleSystem);
+    this.exhaust = new FireParticleSource(this.mesh.position, 1000, 0x882233, 300);
 
     this.missiles_left = 1;
 
     this.bounds = {obj: this};
-  };
-
-  var updateParticles = function(particles) {
-    for (var i = 0; i < NUM_PARTICLES; i++) {
-      var particlePos = particles.vertices[i];
-      if (Math.random() < 0.1) { 
-        particlePos.copy(PlayerShip.mesh.position);
-      }
-
-      particlePos.x += rand(-1, 1);
-      particlePos.y += rand(-1, 1);
-      particlePos.z += rand(-1, 1);
-    }
   };
 
   var update = function(scale) {
@@ -86,26 +58,9 @@ var PlayerShip = (function(){
         this.enabled = false;
       }
     }
-    
-    /*
-    // Wrap around
-    if (this.mesh.position.x < BOUNDS.x) {
-      this.mesh.position.x += BOUNDS.width;
-    }
-    if (this.mesh.position.x > BOUNDS.x + BOUNDS.width) {
-      this.mesh.position.x -= BOUNDS.width;
-    }
-    if (this.mesh.position.z < BOUNDS.y) {
-      this.mesh.position.z += BOUNDS.height;
-    }
-    if (this.mesh.position.z > BOUNDS.y + BOUNDS.height) {
-      this.mesh.position.z -= BOUNDS.height;
-    }
-    */
 
-    updateParticles(this.particles);
+    this.exhaust.update(this.mesh.position);
   };
-
   return {
     init: init,
     update: update,
